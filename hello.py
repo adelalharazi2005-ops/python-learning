5a9d1e174688b57171c26fc3e15ae0f2ed25393e
Adel Ali Mohammed Al-Harazi <adel.alharazi2005@gmail.com>
2026-07-10 12:09:57 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,12 +3,12 @@
 name = "Adel"
 country = "Yemen"
 dream = "AI Engineer"
-age = "20" 
+age = 20
 favorite_sport = 'football'
 
 print("Hello!")
 print("My name is", name)
 print("I am from", country)
 print("My dream is to become an", dream)
-print("I am ", age) 
+print("I am ",age,"years old") 
 print(" My favorite sport is",favorite_sport) 
